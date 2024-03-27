0549c3272bf26c4b34c197ef8f524e1e82269fed
fatih-cakmak <cakmak.35f@gmail.com>
2024-03-27 05:35:08 +0300

    update1

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,2 @@
-import { combineReducers } from "redux";
-import favoritesReducer from "./favoritesReducer";
-import movieReducer from "./movieReducer";
 
-export const reducers = combineReducers({
-  movieReducer: movieReducer,
-  favoritesReducer: favoritesReducer,
-});
 
-export default reducers;
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,11 @@
 import { legacy_createStore as createStore } from "redux";
 import movieReducer from "../store/reducers/movieReducer.js";
-import reducers from "./reducers/index.js";
+import { combineReducers } from "redux";
+import favoritesReducer from "../store/reducers/favoritesReducer.js";
+
+export const reducers = combineReducers({
+  movieReducer: movieReducer,
+  favoritesReducer: favoritesReducer,
+});
 
 export const myStore = createStore(reducers);
